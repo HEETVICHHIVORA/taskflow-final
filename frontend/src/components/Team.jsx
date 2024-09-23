7266350e04feffd793733bed41847000fbf5f04a
heetcodes <heetvic@gmail.com>
2024-09-23 12:04:49 +0530

    delete bytton

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,6 +1,8 @@
 
 import { useContext } from "react";
 import {AppContext} from "../context/AppContext"
+import { MdDelete } from "react-icons/md";
+
 
 export const Team =({group,tasks,settasks,setaddbtn,setaddbtnfortext,createteam})=>{
 
@@ -68,29 +70,24 @@ export const Team =({group,tasks,settasks,setaddbtn,setaddbtnfortext,createteam}
         }
         setloader(false)
     }
-    return <div className="w-full  px-3 hover:bg-slate-100 transition duration-200 cursor-pointer" onClick={showTasks}>
-         <div className="flex p-2 border-b-2">
-            <div className="bg-blue-200 rounded-full w-[12%] justify-center items-center mr-2 hidden lg:flex">
-                   
-            </div>
-            <div>
-                <p className="">
-                    {group.name}
-                </p>
-                <p>
-                    this is the last chat short . 
-                </p>
-            </div>
-         </div>
-         {createteam  && (
-        <div className="mt-2">
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-200"
-            onClick={deleteGroup}
-          >
-            Delete Group
+    return <div className="w-full px-3 hover:bg-slate-100 transition duration-200 cursor-pointer" onClick={showTasks}>
+    <div className="flex p-2 border-b-2">
+      <div className="bg-blue-200 rounded-full w-[12%] justify-center items-center mr-2 hidden lg:flex">
+        {/* You can add some content here if needed */}
+      </div>
+      <div>
+        <p>{group.name}</p>
+        <p>this is the last chat short.</p>
+      </div>
+      
+      {createteam && (
+        <div className="ml-auto mt-2">
+          <button onClick={deleteGroup}>
+            <MdDelete className="text-red-500 text-xl" />
           </button>
         </div>
-        )}
+      )}
     </div>
+  </div>
+  
 }
